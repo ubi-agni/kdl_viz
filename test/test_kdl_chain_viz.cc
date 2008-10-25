9ab215777f73341c02ca522bf880f11d53f2d149
Florian Paul Schmidt <mista.tapas@gmx.net>
2008-10-25 15:02:55 +0000

    starting to work

--- test/test_kdl_chain_viz.cc
+++ test/test_kdl_chain_viz.cc
@@ -1,6 +1,16 @@
 #include "kdl_chain_viz.h"
 
-int main() {
+#include <QApplication>
+#include <kdl/chain.hpp>
 
+int main(int argc, char **argv) {
+	QApplication app(argc, argv);
+
+	ChainPtr chain(new KDL::Chain);
+
+	KDLChainVisualizerWidget w(chain);
+	w.show();
+
+	app.exec();
 }
 
