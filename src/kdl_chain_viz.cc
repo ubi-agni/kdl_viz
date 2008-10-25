b5f598f13da8867bff85ac160523e66146f8c1c1
Florian Paul Schmidt <mista.tapas@gmx.net>
2008-10-25 16:44:21 +0000

    yaya

--- src/kdl_chain_viz.cc
+++ src/kdl_chain_viz.cc
@@ -24,6 +24,7 @@ void KDLChainVisualizerWidget::paintGL()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
+
 	for (unsigned int segment = 0; segment < m_Chain->getNrOfSegments(); ++segment) {
 		// std::cout << "segment " << segment << std::endl;
 
