0362f7efd6bc363f9b5ea2f7463a57d47c32557b
Florian Paul Schmidt <mista.tapas@gmx.net>
2009-05-11 12:05:50 +0000

    - moved stuff from header to .cc

--- src/qkdlchainview.cc
+++ src/qkdlchainview.cc
@@ -1,2 +1,74 @@
 #include "qkdlchainview.h"
 
+namespace KDLCV {
+
+QKDLChainView::QKDLChainView(KDL::Chain *chain, QWidget *parent) :
+	QKDLChainWidget(chain, parent),
+	m_AngleX(M_PI/4.0),
+	m_AngleY(M_PI/4.0),
+	m_Distance(2)
+{
+
+}
+
+void QKDLChainView::initializeGL() {
+	QKDLChainWidget::initializeGL();
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(90.0, ((float)width())/((float)height()), 0.1, 10000.0);
+	gluLookAt(0,m_Distance,0,0,0,0,0,0,1);
+}
+
+void QKDLChainView::paintGL() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(90.0, ((float)width())/((float)height()), 0.1, 10000.0);
+	gluLookAt(0,m_Distance,0,0,0,0,0,0,1);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glRotatef(m_AngleY, 1,0,0);
+	glRotatef(m_AngleX, 0,0,1);
+
+	draw_chain(m_Chain, m_Pose);
+}
+
+void QKDLChainView::mousePressEvent(QMouseEvent *event) {
+	if (event->button() == Qt::LeftButton) {
+		m_LastPosition = event->pos();
+	}
+}
+
+void QKDLChainView::mouseReleaseEvent(QMouseEvent *event) {
+	if (event->button() == Qt::LeftButton) {
+	}
+}
+
+void QKDLChainView::mouseMoveEvent (QMouseEvent * event) {
+	//if (event->button() != Qt::LeftButton) return;
+
+	std::cout << "mouse move: " << m_AngleX << " " << m_AngleY << std::endl;
+	QPoint delta = event->pos() - m_LastPosition;
+
+	m_AngleX += 0.1 * delta.x();
+	m_AngleY += 0.1 * delta.y();
+
+	m_AngleX = fmod(m_AngleX, 360);
+	m_AngleY = m_AngleY > 90 ? 90 : m_AngleY;
+	m_AngleY = m_AngleY < -90 ? -90 : m_AngleY;
+
+	m_LastPosition = event->pos();
+	update();
+}
+
+void QKDLChainView::wheelEvent (QWheelEvent *event)
+{
+	std::cout << "mouse wheel: " << event->delta() << std::endl;
+	m_Distance += (-0.001 * event->delta());
+	update();
+}
+
+} // namespace
--- src/qkdlchainview.h
+++ src/qkdlchainview.h
@@ -30,74 +30,19 @@ namespace KDLCV {
 		QPoint m_LastPosition;
 
 	public:
-		QKDLChainView(KDL::Chain *chain, QWidget *parent = 0) :
-			QKDLChainWidget(chain, parent),
-			m_AngleX(0),
-			m_AngleY(0),
-			m_Distance(2)
-		{
+		QKDLChainView(KDL::Chain *chain, QWidget *parent = 0);
 
-		}
+		virtual void initializeGL();
 
-		virtual void initializeGL() {
-			QKDLChainWidget::initializeGL();
+		virtual void paintGL();
 
-			glMatrixMode(GL_PROJECTION);
-			glLoadIdentity();
-			gluPerspective(90.0, ((float)width())/((float)height()), 0.1, 10000.0);
-			gluLookAt(0,m_Distance,0,0,0,0,0,0,1);
-		}
+		void mousePressEvent(QMouseEvent *event);
 
-		virtual void paintGL() {
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		void mouseReleaseEvent(QMouseEvent *event);
 
-			glMatrixMode(GL_PROJECTION);
-			glLoadIdentity();
-			gluPerspective(90.0, ((float)width())/((float)height()), 0.1, 10000.0);
-			gluLookAt(0,m_Distance,0,0,0,0,0,0,1);
+		void mouseMoveEvent (QMouseEvent * event);
 
-			glMatrixMode(GL_MODELVIEW);
-			glLoadIdentity();
-			glRotatef(m_AngleY, 1,0,0);
-			glRotatef(m_AngleX, 0,0,1);
-
-			draw_chain(m_Chain, m_Pose);
-		}
-
-		void mousePressEvent(QMouseEvent *event) {
-			if (event->button() == Qt::LeftButton) {
-				m_LastPosition = event->pos();
-			}
-		}
-
-		void mouseReleaseEvent(QMouseEvent *event) {
-			if (event->button() == Qt::LeftButton) {
-			}
-		}
-
-		void mouseMoveEvent (QMouseEvent * event) {
-			//if (event->button() != Qt::LeftButton) return;
-
-			std::cout << "mouse move: " << m_AngleX << " " << m_AngleY << std::endl;
-			QPoint delta = event->pos() - m_LastPosition;
-
-			m_AngleX += 0.1 * delta.x();
-			m_AngleY += 0.1 * delta.y();
-
-			m_AngleX = fmod(m_AngleX, 360);
-			m_AngleY = m_AngleY > 90 ? 90 : m_AngleY;
-			m_AngleY = m_AngleY < -90 ? -90 : m_AngleY;
-
-			m_LastPosition = event->pos();
-			update();
-		}
-
-		void wheelEvent (QWheelEvent *event)
-		{
-			std::cout << "mouse wheel: " << event->delta() << std::endl;
-			m_Distance += (-0.001 * event->delta());
-			update();
-		}
+		void wheelEvent (QWheelEvent *event);
 	};
 } // namespace
 #endif
